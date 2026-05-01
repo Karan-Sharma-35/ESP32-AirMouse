a718d2bd99a241b36988158f5d774524381ddcfa
Karan <149451661+Karan-Sharma-35@users.noreply.github.com>
2026-04-30 22:28:42 -0400

    Joystick tested and working, left and right click also implemented
    Implementations updated, tested and ready.

--- AirMouse.ino
+++ AirMouse.ino
@@ -1,11 +1,11 @@
 #include <BleConnectionStatus.h>
 #include <BleMouse.h>
 
-// --- ADDED BROWNOUT OVERRIDES ---
+// Brownout overrides (just in case)
 #include "soc/soc.h"
 #include "soc/rtc_cntl_reg.h"
 
-BleMouse bleMouse("ESP32 G502 Mouse", "Espressif", 100);
+BleMouse bleMouse("Frankenstein", "Espressif", 100);
 
 //using hiletgo to connect to the esp32
 #include <Arduino.h>
@@ -18,17 +18,17 @@ const int PIN_X = 34; //horizaontal axis
 const int PIN_Y = 35; //vertical movement
 
 // //the buttons orientation of joystick being at the top
-// const int PIN_BTN_C = 27; //left green btn (left click)
-// const int PIN_BTN_A = 13; //right green button (right click)
-// const int PIN_BTN_B = 14; //bottom yellow button (MB4/back)
-// const int PIN_BTN_D = 26; //top yel btn (MB5/forward)
+// const int PIN_BTN_C = 27; //bottom yellow button (MB4/back)
+// const int PIN_BTN_A = 13; //top yel btn (MB5/forward)
+const int PIN_BTN_B = 14; //left green btn (left click)
+const int PIN_BTN_D = 26; //right green button (right click)
 
 // //small buttons I will use as home and map ig
 // const int PIN_BTN_E = 25; //bottom button ()
 // const int PIN_BTN_F = 33; //top button (scroll mode toggle)
 
 // //joystick btn
-// const int PIN_BTN_K = 32; //joystick button (middle click)
+const int PIN_BTN_K = 32; //joystick button (middle click)
 
 //NEW: Status LED Pin (Usually GPIO 2)
 const int STATUS_LED = 2;
@@ -36,14 +36,14 @@ const int STATUS_LED = 2;
 //mouse code
 
 //config for the "cursor"
-const int DEADZONE = 150; //Ignores minor drift
+const int DEADZONE = 400; //Ignores minor drift
 const int CENTERVAL = 1930; //joystick center
-const int SENSITIVITY = 60; //Higher sens = slower cursor
-const int SCROLLSPEED = 500; //higher = slower scroll
+const int SENSITIVITY = 120; //Higher sens = slower cursor
 
-//state variables
-bool scrollMode = false; //default non scroll mode
-bool lastToggleState = HIGH; //helper to make sure the scroll toggle just happens without any issues
+//state tracker for the btns
+int lastBtnKState = HIGH; //High meaning that it is unpressed due to INPUT_PULLUP
+int lastBtnBState = HIGH; //High meaning that it is unpressed due to INPUT_PULLUP
+int lastBtnDState = HIGH; //High meaning that it is unpressed due to INPUT_PULLUP
 
 //void setup cause this is C and void means no data type assignment is needed
 void setup(){
@@ -58,6 +58,11 @@ void setup(){
   //Since it is purely analog for now, we can leave it as INPUT
   pinMode(PIN_X, INPUT);
   pinMode(PIN_Y, INPUT);
+
+  //Button config
+  pinMode(PIN_BTN_K, INPUT_PULLUP); //since it wires straight to gnd it needs to be INPUT_PULLUP
+  pinMode(PIN_BTN_B, INPUT_PULLUP); //since it wires straight to gnd it needs to be INPUT_PULLUP
+  pinMode(PIN_BTN_D, INPUT_PULLUP); //since it wires straight to gnd it needs to be INPUT_PULLUP
 
   Serial.println("Starting BLE Testing...");
   bleMouse.begin();
@@ -84,6 +89,8 @@ void loop(){
   //Joystick logic
   if(bleMouse.isConnected()){
 
+    //movement logic
+
     //reading the analog signals of the joystick
     int xReadings = analogRead(PIN_X);
     int yReadings = analogRead(PIN_Y);
@@ -105,6 +112,86 @@ void loop(){
     //making sure signal is sent only if there is actual movement
     if(xMove != 0 || yMove != 0){
       bleMouse.move(xMove, yMove);
+    }
+
+    //middle click logic
+    
+    //updating state based on the input
+    int currBtnKState = digitalRead(PIN_BTN_K);
+
+    //checking if the button state changed after the last loop
+    if(currBtnKState != lastBtnKState){
+
+      if(currBtnKState == LOW){
+        //LOW shows that the button was pressed
+        bleMouse.press(MOUSE_MIDDLE);
+        //commented out to prevent serial monitor clutter
+        // Serial.println("Middle Click has been Pressed");
+      }
+      else{
+        //meaning it is on high and button has been released
+        bleMouse.release(MOUSE_MIDDLE);
+        //commented out to prevent serial monitor clutter
+        // Serial.println("Middle click has been Released");
+      }
+
+      //updating the state to prevent spamming
+      lastBtnKState = currBtnKState;
+
+      //adding a delay for consistency
+      delay(20);
+    }
+
+    //Right click logic
+    
+    //updating state based on the input
+    int currBtnBState = digitalRead(PIN_BTN_B);
+
+    //checking if the button state changed after the last loop
+    if(currBtnBState != lastBtnBState){
+
+      if(currBtnBState == LOW){
+        //LOW shows that the button was pressed
+        bleMouse.press(MOUSE_RIGHT);
+        Serial.println("Right Click has been Pressed");
+      }
+      else{
+        //meaning it is on high and button has been released
+        bleMouse.release(MOUSE_RIGHT);
+        Serial.println("Right click has been Released");
+      }
+
+      //updating the state to prevent spamming
+      lastBtnBState = currBtnBState;
+
+      //adding a delay for consistency
+      delay(20);
+    }
+
+    //Left click logic
+    
+    //updating state based on the input
+    int currBtnDState = digitalRead(PIN_BTN_D);
+
+    //checking if the button state changed after the last loop
+    if(currBtnDState != lastBtnDState){
+
+      if(currBtnDState == LOW){
+        //LOW shows that the button was pressed
+        bleMouse.press(MOUSE_LEFT);
+        Serial.println("Left Click has been Pressed");
+      }
+      else{
+        //meaning it is on high and button has been released
+        bleMouse.release(MOUSE_LEFT);
+        Serial.println("Left click has been Released");
+      }
+
+      //updating the state to prevent spamming
+      lastBtnDState = currBtnDState;
+
+      //adding a delay for consistency
+      delay(20);
     }
 
     //delay for stability
